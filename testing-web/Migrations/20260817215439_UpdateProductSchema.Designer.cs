3d880be3b5a96e4e3fdd2fbe134f0bcb52aa5914
YASHAVkumar <yashavkumar91@gmail.com>
2026-08-18 03:25:57 +0530

    Add database migration

--- testing-web/Migrations/20260817215439_UpdateProductSchema.Designer.cs
+++ testing-web/Migrations/20260817215439_UpdateProductSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using testing_web;
 
@@ -11,9 +12,11 @@ using testing_web;
 namespace testing_web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260817215439_UpdateProductSchema")]
+    partial class UpdateProductSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
